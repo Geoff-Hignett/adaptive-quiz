5fa96f16da9522327c27aa04036bc588fcf2e571
Geoff-Hignett <geoffhig@hotmail.com>
2026-06-26 16:36:52 +0100

    separate admin bug / questions controllers

---
+++ API/Controllers/AdminBugsController.cs
@@ -0,0 +1,39 @@
+﻿using AdaptiveQuiz.Api.Domain;
+using AdaptiveQuiz.Api.Infrastructure;
+using AdaptiveQuiz.Api.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace AdaptiveQuiz.Api.Controllers;
+
+[ApiController]
+[Route("api/admin/bugs")]
+[Authorize]
+public class AdminBugsController : ControllerBase
+{
+    private readonly QuizService _quizService;
+
+    public AdminBugsController(QuizService quizService)
+    {
+        _quizService = quizService;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetBugReports()
+    {
+        var userEmail = User.FindFirst(ClaimTypes.Email)?.Value;
+
+        if (string.IsNullOrEmpty(userEmail))
+            return Unauthorized();
+
+        var user = await _quizService.EnsureUserExists(userEmail);
+
+        if (user.Role != Roles.Admin)
+            return Forbid();
+
+        var reports = await _quizService.GetAllBugReports();
+
+        return Ok(reports);
+    }
+}
